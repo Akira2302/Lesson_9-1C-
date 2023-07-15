1796120fc8691828c71d0e0b71f8cc3849948cdd
Swetlana <akira2302@yandex.ru>
2023-07-15 17:43:55 +0700

    задание 9-1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5-> "2, 4"
+// M = 4; N = 8-> "4, 6, 8"
+
+Console.WriteLine("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void ChetNaturNumber(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    ChetNaturNumber(m + 1, n);
+
+}
+ChetNaturNumber(m, n);
